954401c7b165909988c82cad1660950ed6f609d6
Eric Fernandez <eric.r.fern@gmail.com>
2022-11-09 14:11:40 -0600

    Fixed footer to be at bottom and text to be above

--- src/components/Gear/Gear.jsx
+++ src/components/Gear/Gear.jsx
@@ -7,29 +7,31 @@ const Gear = ({ sortedGear }) => {
     return <div></div>;
   } else
     return (
-      <div className="all-gear">
-        {sortedGear.map((gear) => (
-          <div className="gear-container">
-            <div className="gear-item-pos gear-item-style">
-              <img className="gear-item-image" src={Images}></img>
-              <div className="gear-texts">
-                <div className="gear-item-name-style">{gear.name}</div>
-                <div className="gear-item-descr">
-                  Description: {gear.description}
-                </div>
-                <div className="type">Type: {gear.type}</div>
-                <div className="quality">Quality: {gear.quality}</div>
-                <div className="price">Price: ${gear.price}</div>
-                <div className="date">
-                  Date added: {gear.date_added.slice(0, 10)}
-                </div>
-                <div className="available">
-                  Available: {gear.available.toString()}
+      <div className="all-gear-container">
+        <div className="all-gear">
+          {sortedGear.map((gear) => (
+            <div className="gear-container">
+              <div className="gear-item-pos gear-item-style">
+                <img className="gear-item-image" src={Images}></img>
+                <div className="gear-texts">
+                  <div className="gear-item-name-style">{gear.name}</div>
+                  <div className="gear-item-descr">
+                    Description: {gear.description}
+                  </div>
+                  <div className="type">Type: {gear.type}</div>
+                  <div className="quality">Quality: {gear.quality}</div>
+                  <div className="price">Price: ${gear.price}</div>
+                  <div className="date">
+                    Date added: {gear.date_added.slice(0, 10)}
+                  </div>
+                  <div className="available">
+                    Available: {gear.available.toString()}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     );
 };
