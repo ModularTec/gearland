144732b055b610f36e2ac245cdd863bcc1454fe5
Eric Fernandez <eric.r.fern@gmail.com>
2022-11-09 13:37:40 -0600

    Working css grid for 2 items per page for main gear page

--- src/components/Gear/Gear.jsx
+++ src/components/Gear/Gear.jsx
@@ -9,20 +9,24 @@ const Gear = ({ sortedGear }) => {
     return (
       <div className="all-gear">
         {sortedGear.map((gear) => (
-          <div className="gear-item-pos gear-item-style">
-            <img className="gear-item-image" src={Images}></img>
-            <div className="gear-item-name-style">{gear.name}</div>
-            <div className="gear-item-descr">
-              Description: {gear.description}
-            </div>
-            <div className="type">Type: {gear.type}</div>
-            <div className="quality">Quality: {gear.quality}</div>
-            <div className="price">Price: ${gear.price}</div>
-            <div className="date">
-              Date added: {gear.date_added.slice(0, 10)}
-            </div>
-            <div className="available">
-              Available: {gear.available.toString()}
+          <div className="gear-container">
+            <div className="gear-item-pos gear-item-style">
+              <img className="gear-item-image" src={Images}></img>
+              <div className="gear-texts">
+                <div className="gear-item-name-style">{gear.name}</div>
+                <div className="gear-item-descr">
+                  Description: {gear.description}
+                </div>
+                <div className="type">Type: {gear.type}</div>
+                <div className="quality">Quality: {gear.quality}</div>
+                <div className="price">Price: ${gear.price}</div>
+                <div className="date">
+                  Date added: {gear.date_added.slice(0, 10)}
+                </div>
+                <div className="available">
+                  Available: {gear.available.toString()}
+                </div>
+              </div>
             </div>
           </div>
         ))}
