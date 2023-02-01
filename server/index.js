ab65cdffd0d19712c004d8de5d7f776c97223504
Eric Fernandez <eric.r.fern@gmail.com>
2023-02-01 12:23:26 -0600

    changed port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const db = require("../db/queries");
 const dotenv = require(‘dotenv’).config();
 // const PORT = process.env.PORT || 5000;
 // const PORT = parseInt(process.env.PORT_NUMBER || 5000);
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const cors = require("cors");
 
 app.use(cors({ origin: true }));
@@ -30,6 +30,4 @@ app.delete("/gear/:id", db.deleteGear);
 //   console.log(PORT, `App running on PORT ${PORT}.`);
 // });
 
-app.listen(`0.0.0.0:${process.env.PORT || 3000}`, () => {
-  console.log("server listening on port 3000!");
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
