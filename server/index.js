946700fb5dcfd7ffabcdf1769837b2ea184797df
Eric Fernandez <eric.r.fern@gmail.com>
2023-02-03 14:56:36 -0600

    added .listen change

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const db = require("../db/queries");
 // const dotenv = require(‘dotenv’).config();
-// const PORT = process.env.PORT || 4000;
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+//const PORT = 4000;
 const cors = require("cors");
 
 app.use(cors({ origin: true }));
@@ -25,4 +25,6 @@ app.put("/gear/:id", db.updateGear);
 
 app.delete("/gear/:id", db.deleteGear);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(`0.0.0.0:${PORT}`, () =>
+  console.log(`Server running on port ${PORT}`)
+);
