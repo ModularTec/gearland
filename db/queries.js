0c57f3a80032c3e08e5b1f603db336bd09aa2bf1
Eric Fernandez <eric.r.fern@gmail.com>
2023-02-09 13:09:12 -0600

    returned PORT to PGPORT in queries.js

--- db/queries.js
+++ db/queries.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   host: process.env.PGHOST,
   database: process.env.PGDATABSE,
   password: process.env.PGPASSWORD,
-  port: process.env.PORT,
+  port: process.env.PGPORT,
 });
 
 // GET all gear
