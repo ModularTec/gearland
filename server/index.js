251b756787b07252c72f260e71db71bcdc3c5c52
Eric Fernandez <eric.r.fern@gmail.com>
2023-02-21 09:50:14 -0600

    changed index

--- server/index.js
+++ server/index.js
@@ -11,9 +11,11 @@ app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (request, response) => {
-  response.json({ info: "this is the get for /" });
-});
+const path = require("path");
+app.use("/", express.static(path.join(__dirname, "public")));
+// app.get("/", (request, response) => {
+//   response.json({ info: "this is the get for /" });
+// });
 
 app.get("/gear", db.getAllGear);
 
