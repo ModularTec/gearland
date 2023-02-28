d02d20fbefa36d19793c246f26486262dc65c414
Eric Fernandez <eric.r.fern@gmail.com>
2023-02-28 09:39:42 -0600

    changed app.get in index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const db = require("../db/queries");
 // const dotenv = require(‘dotenv’).config();
-const PORT_NUM = process.env.PORT;
+const PORT_NUM = process.env.PORT || 5000;
 //const PORT = 4000;
 const cors = require("cors");
 
@@ -11,11 +11,14 @@ app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const path = require("path");
-app.use("/", express.static(path.join(__dirname, "public")));
-// app.get("/", (request, response) => {
-//   response.json({ info: "this is the get for /" });
-// });
+// const path = require("path");
+// app.use("/", express.static(path.join(__dirname, "public")));
+// response.json({ info: "this is the get for /" });
+
+app.get("/", (request, response) => {
+  response.sendFile(path.resolve(__dirname, "public", "index.html"));
+  // response.json({ info: "this is the get for /" });
+});
 
 app.get("/gear", db.getAllGear);
 
