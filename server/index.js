82d5721f56298b8a13279d0daeb10a49493b9896
Eric Fernandez <eric.r.fern@gmail.com>
2023-03-03 12:52:04 -0600

    tried new express line with ./public

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express.static();
+const app = express();
 const db = require("../db/queries");
 // const dotenv = require(‘dotenv’).config();
 const PORT_NUM = process.env.PORT || 5000;
 //const PORT = 4000;
 const cors = require("cors");
 //app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.use(cors({ origin: true }));
 app.use(bodyParser.json());
