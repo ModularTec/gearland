44c13673352538bdc2aa6829600e1e0d90be2fae
Eric Fernandez <eric.r.fern@gmail.com>
2023-03-03 13:27:18 -0600

    moved app.listen above app.use statements

--- server/index.js
+++ server/index.js
@@ -26,6 +26,8 @@ app.put("/gear/:id", db.updateGear);
 
 app.delete("/gear/:id", db.deleteGear);
 
+app.listen(PORT_NUM, () => console.log(`Server running on port ${PORT_NUM}`));
+
 // app.use
 app.use(cors({ origin: true }));
 app.use(bodyParser.json());
@@ -33,5 +35,3 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 // app.use("/", express.static(path.join(__dirname, "public")));
 // response.json({ info: "this is the get for /" });
-
-app.listen(PORT_NUM, () => console.log(`Server running on port ${PORT_NUM}`));
